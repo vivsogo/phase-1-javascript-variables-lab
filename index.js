3c607eb5d718dd8deaf66fc48dc0081100bed9fb
Vivian Gomez <viviangomez0@gmail.com>
2021-10-20 20:51:18 -0400

    commit tree 82a72822c4df8dd8b9a20552133169846bd85077

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const companyName = 'Scuber';
 let mostProfitableNeighborhood ='Chelsea';
 let companyCeo = 'Susan Smith';
+
+function saturdayFun(Activity="Roller-skate"){
+    console.log(`This Saturday,I want to ${Activity}`);
+}
+saturdayFun()
